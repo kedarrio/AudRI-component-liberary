4c4241ead309b8e2b83efd62acd6c99cafc1095c
Kedarr <kedarmulay1@gmail.com>
2026-04-13 21:21:42 +0530

    Add basename for GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { ToastProvider, Button } from '@lib';
 const App = () => {
   return (
     <ToastProvider>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <BrowserRouter 
+        basename="/AudRI-component-liberary"
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
         <Routes>
           <Route path="/*" element={<LibraryPage />} />
         </Routes>
